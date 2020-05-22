f87287a6ed6195b667930f4d32c7dd1f44a3b45d
ark229 <40047791+ark229@users.noreply.github.com>
2020-05-21 22:57:24 -0400

    Create loops_greetings.py

---
+++ loops_greetings.py
@@ -0,0 +1,17 @@
+###
+Create a function named add_greetings() which takes a list of strings named names as a parameter.
+In the function, create an empty list that will contain each greeting. Add the string "Hello, " in front of each name in names and append the greeting to the list.
+Return the new list containing the greetings.
+###
+
+#Write your function here
+
+def add_greetings(names):
+  new_greetings = []
+  for name in names:
+    new_greetings.append("Hello: " + name)
+  return new_greetings
+
+
+#Uncomment the line below when your function is done
+print(add_greetings(["Owen", "Max", "Sophie"]))
